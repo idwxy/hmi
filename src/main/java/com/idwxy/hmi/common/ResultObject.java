214c37cfe41d0e22ff3316eb93f6c8a045355b59
idwxy <idwxy@live.com>
2019-09-20 15:27:47 +0800

    建立封装后端返回数据

---
+++ src/main/java/com/idwxy/hmi/common/ResultObject.java
@@ -0,0 +1,54 @@
+package com.idwxy.hmi.common;
+
+public class ResultObject {
+    // 后台信息
+    private Integer code;
+    // 相关信息
+    private String msg;
+    // 结果
+    private Object result;
+
+    // 构造函数
+    public ResultObject() {
+        super();
+    }
+
+    public ResultObject(Integer code, String msg, Object result) {
+        super();
+        this.code = code;
+        this.msg = msg;
+        this.result = result;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    // 重写 toString 方法
+    @Override
+    public String toString() {
+        return "ResultObject [code=" + code +
+                ", msg=" + msg +
+                ", result=" + result + "]";
+    }
+}
