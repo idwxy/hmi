3c71e114546984b33589b442bb86e67ecb88b077
idwxy <idwxy@live.com>
2019-09-22 17:18:12 +0800

    增加注释

--- src/main/java/com/idwxy/hmi/HmiApplication.java
+++ src/main/java/com/idwxy/hmi/HmiApplication.java
@@ -1,9 +1,11 @@
 package com.idwxy.hmi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.idwxy.hmi.dao")
 public class HmiApplication {
 
 	public static void main(String[] args) {
